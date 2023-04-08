0bc83f16ce62f78b1943feb5c335359ae0e4fccb
Moscarde <theizera@gmail.com>
2023-04-08 17:35:24 -0300

    08.04_2

--- 15_GET_A_PET/backend/controllers/UserController.js
+++ 15_GET_A_PET/backend/controllers/UserController.js
@@ -1,7 +1,11 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
+// Helpers
 const createUserToken = require('../helpers/create-user-token')
+const getToken = require('../helpers/get-token')
+const getUserByToken = require('../helpers/get-user-by-token')
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -86,13 +90,104 @@ module.exports = class UserController {
         const checkPassword = await bcrypt.compare(password, user.password)
 
         if (!checkPassword) {
-            res.status(422).json({ message: 'Senha inválida'})
+            res.status(422).json({ message: 'Senha inválida' })
+            return
         }
 
         // Create authentication token
         await createUserToken(user, req, res)
- 
 
+
+    }
+
+    static async checkUser(req, res) {
+        let currentUser
+
+        if (req.headers.authorization) {
+            const token = getToken(req)
+            const decoded = jwt.verify(token, 'nossosecret')
+
+            currentUser = await User.findById(decoded.id)
+            currentUser.password = undefined
+        } else {
+            currentUser = null
+        }
+
+        res.status(200).send(currentUser)
+    }
+
+    static async getUserById(req, res) {
+        const id = req.params.id
+
+        try {
+            const user = await User.findById(id).select('-password')
+            res.status(200).json({ user })
+        } catch (error) {
+            res.status(422).json({ message: 'Usuário não encontrado', error })
+        }
+
+
+    }
+
+    static async editUser(req, res) {
+        // Check if user exists
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+        console.log(user)
+
+        // Validations
+        const { name, email, phone, password, confirmPassword } = req.body
+        let image = ''
+
+        if (!name) {
+            res.status(422).json({ message: 'O nome é obrigatório!' })
+            return
+        }
+        user.name = name
+
+        if (!email) {
+            res.status(422).json({ message: 'O email é obrigatório!' })
+            return
+        }
+
+        // check if email has already taken
+        const userExists = await User.findOne({ email: email })
+
+        if (user.email !== email && userExists) {
+            res.status(422).json({ message: 'Email em uso' })
+            return
+        }
+        user.email = email
+
+        if (!phone) {
+            res.status(422).json({ message: 'O telefone é obrigatório!' })
+            return
+        }
+        user.phone = phone
+
+        if (password !== confirmPassword) {
+            res.status(422).json({ message: 'A senha e a confirmação de senha precisam ser iguais!' })
+            return
+        }
+        else if (password === confirmPassword && password != null) {
+            // Create a password
+            const salt = await bcrypt.genSalt(12)
+            const passwordHash = await bcrypt.hash(password, salt)
+            user.password = passwordHash
+        }
+
+        try {
+            // Return user updated data
+            await User.findOneAndUpdate(
+                { _id: user._id },
+                { $set: user },
+                { new: true }
+            )
+            res.status(200).json({ message: 'Usuário atualizado com sucesso' })
+        }
+        catch (error) {
+            res.status(500).json({ message: error })
+        }
     }
 
 }
--- 15_GET_A_PET/backend/routes/UserRoutes.js
+++ 15_GET_A_PET/backend/routes/UserRoutes.js
@@ -1,8 +1,17 @@
 const router = require('express').Router()
 
+// Middleware
+const verifyToken = require('../helpers/verify-token')
+
+// Controller
 const UserController = require('../controllers/UserController')
 
+// Routes
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
+router.get('/checkuser', UserController.checkUser)
+router.get('/:id', UserController.getUserById)
+router.patch('/edit/:id', verifyToken, UserController.editUser)
+
 
 module.exports = router
