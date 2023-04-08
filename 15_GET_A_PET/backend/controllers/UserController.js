5ff37780dc6ee1d9d4be2d58d8a3343e73370563
Moscarde <theizera@gmail.com>
2023-04-08 07:10:26 -0300

    08.04

--- 15_GET_A_PET/backend/controllers/UserController.js
+++ 15_GET_A_PET/backend/controllers/UserController.js
@@ -1,7 +1,98 @@
 const User = require('../models/User')
+const bcrypt = require('bcrypt')
+
+const createUserToken = require('../helpers/create-user-token')
 
 module.exports = class UserController {
     static async register(req, res) {
-        res.json({ message: 'Olá Get A Pet' })
+        const { name, email, phone, password, confirmPassword } = req.body
+
+        // Validations
+        if (!name) {
+            res.status(422).json({ message: 'O nome é obrigatório!' })
+            return
+        }
+        if (!email) {
+            res.status(422).json({ message: 'O email é obrigatório!' })
+            return
+        }
+        if (!phone) {
+            res.status(422).json({ message: 'O telefone é obrigatório!' })
+            return
+        }
+        if (!password) {
+            res.status(422).json({ message: 'A senha é obrigatória!' })
+            return
+        }
+        if (!confirmPassword) {
+            res.status(422).json({ message: 'A confirmação de senha é obrigatória!' })
+            return
+        }
+        if (password !== confirmPassword) {
+            res.status(422).json({ message: 'A senha e a confirmação de senha precisam ser iguais!' })
+            return
+        }
+
+        // Check if user exists
+        const userExists = await User.findOne({ email: email })
+
+        if (userExists) {
+            res.status(422).json({ message: 'Por favor, utilize outro e-mail' })
+            return
+        }
+
+        // Create a password
+        const salt = await bcrypt.genSalt(12)
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        // Create a user
+        const user = new User({
+            name,
+            email,
+            phone,
+            password: passwordHash
+        })
+
+        try {
+            const newUser = await user.save()
+            await createUserToken(newUser, req, res)
+        } catch (error) {
+            res.status(500).json({ message: error })
+        }
     }
+
+    static async login(req, res) {
+        const { email, password } = req.body
+
+        // Validations
+        if (!email) {
+            res.status(422).json({ message: 'O email é obrigatório!' })
+            return
+        }
+        if (!password) {
+            res.status(422).json({ message: 'A senha é obrigatória!' })
+            return
+        }
+
+        // Check if user exists
+        const user = await User.findOne({ email: email })
+
+        if (!user) {
+            res.status(422).json({ message: 'Não há usuário cadastrado com esse email!' })
+            return
+        }
+
+        // Check if password match with db password
+        const checkPassword = await bcrypt.compare(password, user.password)
+
+        if (!checkPassword) {
+            res.status(422).json({ message: 'Senha inválida'})
+        }
+
+        // Create authentication token
+        await createUserToken(user, req, res)
+ 
+
+    }
+
 }
