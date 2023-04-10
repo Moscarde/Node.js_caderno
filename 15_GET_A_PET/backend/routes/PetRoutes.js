a79a546c210fb5be983fbc4c77096e9b846c2e03
Moscarde <theizera@gmail.com>
2023-04-10 20:24:00 -0300

    10.04

--- 15_GET_A_PET/backend/routes/PetRoutes.js
+++ 15_GET_A_PET/backend/routes/PetRoutes.js
@@ -11,5 +11,13 @@ const PetController = require('../controllers/PetController')
 router.post('/create', verifyToken, imageUpload.array('images'), PetController.create)
 router.get('/', PetController.getAll)
 router.get('/mypets', verifyToken, PetController.getAllUserPets)
+router.get('/myadoptions', verifyToken, PetController.getAllUserAdoptions)
+router.get('/:id', PetController.getPetById)
+router.delete('/:id', verifyToken, PetController.removePetById)
+router.patch('/:id', verifyToken, imageUpload.array('images'), PetController.updatePetBy)
+router.patch('/schedule/:id', verifyToken, PetController.schedule)
+router.patch('/conclude/:id', verifyToken, PetController.concludeAdoption)
+
+
 
 module.exports = router
