435903928d3e5f1808380ac279db4e9deb7e84a6
Moscarde <theizera@gmail.com>
2023-04-11 03:21:07 -0300

    11.04

---
+++ 15_GET_A_PET/frontend/src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <p><span className="bold">Get A Pet &copy; 2023</span></p>
+        </footer>
+    )
+}
+
+export default Footer
